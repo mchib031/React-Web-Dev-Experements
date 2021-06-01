1e29926e54a1a65cfb099bad793b070c578d695e
mchib031 <71799789+mchib031@users.noreply.github.com>
2021-06-01 11:43:02 -0400

    fixed

--- Lab2/scripts/groceries.js
+++ Lab2/scripts/groceries.js
@@ -1,106 +1,126 @@
+	 
 
-// Array of products, each product is an object with different fieldset
-// A set of ingredients should be added to products
+const selectedRestrictions = new Set()
 
 var products = [
 	{
 		name: "Brocoli",
-		Organic: true,
 		LactoseFree: true,
 		NutFree: true,
+		isOrganic:true,
 		price: 1.99
-	},
-	{
-		name: "Apple",
-		LactoseFree: true,
-		NutFree: true,
-		Organic:false,
-		price: 2
-	},
-	{
-		name: "Salmon",
-		Organic: true,
-		LactoseFree: true,
-		NutFree: true,
-		price: 10.00
-	},
-	{
-		name: "Milk",
-		Organic: true,
-		LactoseFree: false,
-		NutFree: true,
-		price: 6
 	},
 	{
 		name: "Bread",
 		LactoseFree: false,
 		NutFree: false,
-		Organic:true,
+		isOrganic:true,
 		price: 2.35
 	},
 	{
 		name: "Tomato",
 		LactoseFree: true,
 		NutFree: true,
-		Organic:true,
-		price: 4
+		isOrganic:true,
+		price: 3.90
 	},
 	{
-		name: "Steak",
-		Organic: true,
-		LactoseFree: true,
+		name: "Organic Cheese",
+		LactoseFree: false,
 		NutFree: true,
-		price: 14.99
+		isOrganic:true,
+		price: 3.49
 	},
 	{
 		name: "Cheese",
 		LactoseFree: false,
 		NutFree: true,
-		Organic:false,
-		price: 3
+		isOrganic:false,
+		price: 2.47
 	},
 	{
-		name: "Eggs",
-		Organic: true,
+		name: "Salmon",
 		LactoseFree: true,
 		NutFree: true,
-		price: 14.99
+		isOrganic:true,
+		price: 10.00
+	},
+	{
+		name: "Apple",
+		LactoseFree: true,
+		NutFree: true,
+		isOrganic:false,
+		price: 1.79
+	},
+	{
+		name: "Milk",
+		LactoseFree: false,
+		NutFree: true,
+		isOrganic:true,
+		price: 8.50
+	},
+	{
+		name: "Almonds",
+		LactoseFree: true,
+		NutFree: false,
+		isOrganic:true,
+		price: 2.00
+	},
+	{
+		name: "Rice",
+		LactoseFree: true,
+		NutFree: true,
+		isOrganic:false,
+		price: 13.00
 	},
 	{
 		name: "Honey",
 		LactoseFree: true,
 		NutFree: false,
 		isOrganic:false,
-		price: 7
-	}
-
+		price: 5.99
+	},
+	{
+		name: "Avacado",
+		LactoseFree: true,
+		NutFree: true,
+		isOrganic:false,
+		price: 3.99
+	},
+	{
+		name: "Strawberry",
+		LactoseFree: true,
+		NutFree: true,
+		isOrganic:true,
+		price: 2.30
+	},
+	{
+		name: "Banana",
+		LactoseFree: true,
+		NutFree: true,
+		isOrganic:true,
+		price: 1.23
+	},
 ];
+	
 
-
-
-// given restrictions provided, make a reduced list of products
-// prices should be included in this list, as well as a sort based on price
-
-function restrictListProducts(prods, restriction) {
-	let product_names = [];
-	for (let i=0; i<prods.length; i+=1) {
-		if ((restriction == "NutFree") && (prods[i].NutFree == true)){
-			product_names.push(prods[i].name);
-		}
-		else if ((restriction == "LactoseFree") && (prods[i].LactoseFree == true)){
-			product_names.push(prods[i].name);
-		}
-		else if ((restriction == "Organic") && (prods[i].Organic == true)){
-			product_names.push(prods[i].name);
-		}
-		else if (restriction == "None"){
-			product_names.push(prods[i].name);
-		}
+function restrictListProducts(prods) {
+	let filteredProducts = [];
+	for (let i = 0; i < prods.length; i += 1) {
+	  if ((selectedRestrictions.has("Lactose-Free")) && (prods[i].LactoseFree == false)){
+		continue;
+	  }
+	  if ((selectedRestrictions.has("Nut-Free")) && (prods[i].NutFree == false)){
+		continue;
+	  }
+	  if ((selectedRestrictions.has("Organic")) && (prods[i].isOrganic == false)){
+		continue;
+	  }
+	  filteredProducts.push(prods[i]);
 	}
-	return product_names;
-}
+	return filteredProducts;
+  }
 
-// Calculate the total price of items, with received parameter being a list of products
 function getTotalPrice(chosenProducts) {
 	totalPrice = 0;
 	for (let i=0; i<products.length; i+=1) {
--- Lab2/scripts/main.js
+++ Lab2/scripts/main.js
@@ -1,70 +1,44 @@
 
-// This function is called when any of the tab is clicked
-// It is adapted from https://www.w3schools.com/howto/howto_js_tabs.asp
+const allItems = restrictListProducts(products)
+renderProductList(allItems)
+
 
 function openInfo(evt, tabName) {
 
-	// Get all elements with class="tabcontent" and hide them
 	tabcontent = document.getElementsByClassName("tabcontent");
 	for (i = 0; i < tabcontent.length; i++) {
 		tabcontent[i].style.display = "none";
 	}
 
-	// Get all elements with class="tablinks" and remove the class "active"
 	tablinks = document.getElementsByClassName("tablinks");
 	for (i = 0; i < tablinks.length; i++) {
 		tablinks[i].className = tablinks[i].className.replace(" active", "");
 	}
 
-	// Show the current tab, and add an "active" class to the button that opened the tab
-	document.getElementById(tabName).style.display = "block";
+	document.getElementById(tabName).style.display = "flex";
 	evt.currentTarget.className += " active";
 
 }
 
 
-
-// generate a checkbox list from a list of products
-// it makes each product name as the label for the checkbos
-
-function populateListProductChoices(slct1, slct2) {
+function populateListProductChoices(slct1) {
     var s1 = document.getElementById(slct1);
-    var s2 = document.getElementById(slct2);
+    var s2 = document.getElementById('displayProduct');
 
-	// s2 represents the <div> in the Products tab, which shows the product list, so we first set it empty
     s2.innerHTML = "";
 
-	// obtain a reduced list of products based on restrictions
+	if (selectedRestrictions.has(s1.value)) {
+		selectedRestrictions.delete(s1.value)
+	  } else {
+		selectedRestrictions.add(s1.value)
+	  }
+
     var optionArray = restrictListProducts(products, s1.value);
 
-	// for each item in the array, create a checkbox element, each containing information such as:
-	// <input type="checkbox" name="product" value="Bread">
-	// <label for="Bread">Bread/label><br>
 
-	for (i = 0; i < optionArray.length; i++) {
-
-		var productName = optionArray[i];
-		// create the checkbox and add in HTML DOM
-		var checkbox = document.createElement("input");
-		checkbox.type = "checkbox";
-		checkbox.name = "product";
-		checkbox.value = productName;
-		s2.appendChild(checkbox);
-
-		// create a label for the checkbox, and also add in HTML DOM
-		var label = document.createElement('label')
-		label.htmlFor = productName;
-		label.appendChild(document.createTextNode(productName));
-		s2.appendChild(label);
-
-		// create a breakline node and add in HTML DOM
-		s2.appendChild(document.createElement("br"));
-	}
+	renderProductList(optionArray)
 }
 
-// This function is called when the "Add selected items to cart" button in clicked
-// The purpose is to build the HTML to be displayed (a Paragraph)
-// We build a paragraph to contain the list of selected items, and the total price
 
 function selectedItems(){
 
@@ -74,9 +48,8 @@ function selectedItems(){
 	var c = document.getElementById('displayCart');
 	c.innerHTML = "";
 
-	// build list of selected item
 	var para = document.createElement("P");
-	para.innerHTML = "You selected : ";
+	para.innerHTML = "You have selected:";
 	para.appendChild(document.createElement("br"));
 	for (i = 0; i < ele.length; i++) {
 		if (ele[i].checked) {
@@ -86,8 +59,36 @@ function selectedItems(){
 		}
 	}
 
-	// add paragraph and total price
 	c.appendChild(para);
-	c.appendChild(document.createTextNode("Total Price is " + getTotalPrice(chosenProducts)));
+	c.appendChild(document.createTextNode("Total Price is: " + getTotalPrice(chosenProducts)+" $"));
 
 }
+
+function priceFilter(a,b){
+	return a.price > b.price ? 1:-1;
+}
+
+function renderProductList(optionArray){
+	var s2 = document.getElementById('displayProduct');
+	const sortedOptionArray = optionArray.sort(priceFilter);
+	for (i = 0; i < sortedOptionArray.length; i++) {
+
+	  var productName = sortedOptionArray[i].name;
+	  var productPrice = sortedOptionArray[i].price;
+
+	  var checkbox = document.createElement("input");
+	  checkbox.type = "checkbox";
+	  checkbox.name = "product";
+	  checkbox.value = productName;
+	  s2.appendChild(checkbox);
+
+	  formattedPrice = (Math.round(productPrice * 100) / 100).toFixed(2);
+
+	  var label = document.createElement('label')
+	  label.htmlFor = productName;
+	  label.appendChild(document.createTextNode(`${productName} - $${formattedPrice}`));
+	  s2.appendChild(label);
+
+	  s2.appendChild(document.createElement("br"));
+	}
+  }
