32a03bf46fb1ecc2509dbe49c2e7a1199e005af7
mchib031 <71799789+mchib031@users.noreply.github.com>
2021-06-06 19:37:14 -0400

    Update groceries.js

--- Lab3/scripts/groceries.js
+++ Lab3/scripts/groceries.js
@@ -132,36 +132,36 @@ var products = [
 	{
 		name: "Pho",
 		picture: "pictures/pho.jpg",
-		LactoseFree: true,
-		NutFree: true,
-		isOrganic:true,
+		LactoseFree: false,
+		NutFree: false,
+		isOrganic:false,
 		type : "Vietnamese",
 		price: 9.50
 	},
 	{
 		name: "Cha Ca",
 		picture: "pictures/chaca.jpg",
-		LactoseFree: true,
-		NutFree: true,
-		isOrganic:true,
+		LactoseFree: false,
+		NutFree: false,
+		isOrganic:false,
 		type : "Vietnamese",
 		price: 7.50
 	},
 	{
 		name: "Cao Lau",
 		picture: "pictures/caolau.jpg",
-		LactoseFree: true,
-		NutFree: true,
-		isOrganic:true,
+		LactoseFree: false,
+		NutFree: false,
+		isOrganic:false,
 		type : "Vietnamese",
 		price: 10.50
 	},
 	{
 		name: "Banh khot",
 		picture: "pictures/banhkhot.jpg",
-		LactoseFree: true,
-		NutFree: true,
-		isOrganic:true,
+		LactoseFree: false,
+		NutFree: false,
+		isOrganic:false,
 		type : "Vietnamese",
 		price: 14.00
 	},
