eaeeab584006a599889099733da428eb59742074
mchib031 <71799789+mchib031@users.noreply.github.com>
2021-06-06 13:50:56 -0400

    Lab 3 upload

---
+++ Lab3/scripts/groceries.js
@@ -0,0 +1,160 @@
+
+
+const selectedRestrictions = new Set()
+
+var products = [
+	{
+		name: "Brocoli",
+		picture: "pictures/brocoli.jpg",
+		LactoseFree: true,
+		NutFree: true,
+		isOrganic:true,
+		type : "Vegetable",
+		price: 2.00
+	},
+	{
+		name: "Bread",
+		picture: "pictures/bread.jpg",
+		LactoseFree: false,
+		NutFree: false,
+		isOrganic:true,
+		type : "Carbs",
+		price: 2.00
+	},
+	{
+		name: "Tomato",
+		picture: "pictures/tomato.jpg",
+		LactoseFree: true,
+		NutFree: true,
+		isOrganic:true,
+		type : "Vegetable",
+		price: 4.00
+	},
+	{
+		name: "Organic Cheese",
+		picture: "pictures/organic_cheese.jpg",
+		LactoseFree: false,
+		NutFree: true,
+		isOrganic:true,
+		type : "Dairy",
+		price: 3.50
+	},
+	{
+		name: "Cheese",
+		picture: "pictures/cheese.jpg",
+		LactoseFree: false,
+		NutFree: true,
+		isOrganic:false,
+		type : "Dairy",
+		price: 2.50
+	},
+	{
+		name: "Salmon",
+		picture: "pictures/salmon.jpg",
+		LactoseFree: true,
+		NutFree: true,
+		isOrganic:true,
+		type : "Protein",
+		price: 10.00
+	},
+	{
+		name: "Apple",
+		picture: "pictures/apple.jpg",
+		LactoseFree: true,
+		NutFree: true,
+		isOrganic:false,
+		type : "Fruit",
+		price: 1.70
+	},
+	{
+		name: "Milk",
+		picture: "pictures/milk.jpg",
+		LactoseFree: false,
+		NutFree: true,
+		isOrganic:true,
+		type : "Dairy",
+		price: 8.50
+	},
+	{
+		name: "Almonds",
+		picture: "pictures/almonds.jpg",
+		LactoseFree: true,
+		NutFree: false,
+		isOrganic:true,
+		type : "Fat",
+		price: 2.00
+	},
+	{
+		name: "Rice",
+		picture: "pictures/rice.jpg",
+		LactoseFree: true,
+		NutFree: true,
+		isOrganic:false,
+		type : "Carbs",
+		price: 13.00
+	},
+	{
+		name: "Honey",
+		picture: "pictures/honey.jpg",
+		LactoseFree: true,
+		NutFree: false,
+		isOrganic:false,
+		type : "Fat",
+		price: 4.00
+	},
+	{
+		name: "Avacado",
+		picture: "pictures/avocado.jpg",
+		LactoseFree: true,
+		NutFree: true,
+		isOrganic:false,
+		type : "Fruit",
+		price: 4.00
+	},
+	{
+		name: "Strawberry",
+		picture: "pictures/strawberry.jpg",
+		LactoseFree: true,
+		NutFree: true,
+		isOrganic:true,
+		type : "Fruit",
+		price: 2.50
+	},
+	{
+		name: "Banana",
+		picture: "pictures/banana.jpg",
+		LactoseFree: true,
+		NutFree: true,
+		isOrganic:true,
+		type : "Fruit",
+		price: 1.50
+	},
+];
+
+
+function restrictListProducts(prods) {
+	let filteredProducts = [];
+	for (let i = 0; i < prods.length; i += 1) {
+	  if ((selectedRestrictions.has("Lactose-Free")) && (prods[i].LactoseFree == false)){
+		continue;
+	  }
+	  if ((selectedRestrictions.has("Nut-Free")) && (prods[i].NutFree == false)){
+		continue;
+	  }
+	  if ((selectedRestrictions.has("Organic")) && (prods[i].isOrganic == false)){
+		continue;
+	  }
+	  filteredProducts.push(prods[i]);
+	}
+	return filteredProducts;
+  }
+
+function getTotalPrice(chosenProducts) {
+	totalPrice = 0;
+	for (let i=0; i<products.length; i+=1) {
+		if (chosenProducts.indexOf(products[i].name) > -1){
+			totalPrice += products[i].price;
+		}
+	}
+	return totalPrice;
+}
---
+++ Lab3/scripts/main.js
@@ -0,0 +1,220 @@
+
+const allItems = restrictListProducts(products)
+renderProductList(allItems)
+
+var pro = document.getElementsByClassName("Proteins");
+var fruits = document.getElementsByClassName("Fruits");
+var veg = document.getElementsByClassName("Vegetables");
+var dairy = document.getElementsByClassName("Dairy");
+var fats = document.getElementsByClassName("Fats");
+var carbs = document.getElementsByClassName("Carbs");
+
+function openInfo(evt, tabName) {
+
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = "none";
+	}
+
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+
+	if (tabName == "Products") {
+		document.getElementById(tabName).style.display = "block";
+	}
+	else {
+		document.getElementById(tabName).style.display = "flex";
+	}
+	evt.currentTarget.className += " active";
+
+}
+
+
+function populateListProductChoices(slct1) {
+    var s1 = document.getElementById(slct1);
+    var s2 = document.getElementById('displayProduct');
+
+    s2.innerHTML = "";
+
+	if (selectedRestrictions.has(s1.value)) {
+		selectedRestrictions.delete(s1.value)
+	  } else {
+		selectedRestrictions.add(s1.value)
+	  }
+
+    var optionArray = restrictListProducts(products, s1.value);
+
+
+	renderProductList(optionArray)
+}
+
+
+function selectedRes() {
+	openInfo(event, "Products");
+}
+
+function openCarbs() {
+	hideElems();
+	for (var i = 0; i < carbs.length; i++) {
+		carbs[i].style.display = "flex";
+	}
+}
+
+function openFruits() {
+	hideElems();
+	for (var i = 0; i < fruits.length; i++) {
+		fruits[i].style.display = "flex";
+	}
+}
+
+function openVeg() {
+	hideElems();
+	for (var i = 0; i < veg.length; i++) {
+		veg[i].style.display = "flex";
+	}
+}
+
+function openDairy() {
+	hideElems();
+	for (var i = 0; i < dairy.length; i++) {
+		dairy[i].style.display = "flex";
+	}
+}
+
+function openProtein() {
+	hideElems();
+	for (var i = 0; i < pro.length; i++) {
+		pro[i].style.display = "flex";
+	}
+}
+
+function openFat() {
+	hideElems();
+	for (var i = 0; i < fats.length; i++) {
+		fats[i].style.display = "flex";
+	}
+}
+
+function hideElems() {
+	for (var i = 0; i < fruits.length; i++) {
+		fruits[i].style.display = "none";
+	}
+	for (var i = 0; i < veg.length; i++) {
+		veg[i].style.display = "none";
+	}
+	for (var i = 0; i < pro.length; i++) {
+		pro[i].style.display = "none";
+	}
+	for (var i = 0; i < dairy.length; i++) {
+		dairy[i].style.display = "none";
+	}
+	for (var i = 0; i < fats.length; i++) {
+		fats[i].style.display = "none";
+	}
+	for (var i = 0; i < carbs.length; i++) {
+		carbs[i].style.display = "none";
+	}
+}
+
+function selectedItems(){
+
+	var ele = document.getElementsByName("product");
+	var chosenProducts = [];
+
+	var c = document.getElementById('displayCart');
+	c.innerHTML = "";
+
+	var para = document.createElement("P");
+	para.innerHTML = "You have selected:";
+	para.appendChild(document.createElement("br"));
+	for (i = 0; i < ele.length; i++) {
+		if (ele[i].checked) {
+			para.appendChild(document.createTextNode(ele[i].value));
+			para.appendChild(document.createElement("br"));
+			chosenProducts.push(ele[i].value);
+		}
+	}
+
+	c.appendChild(para);
+	c.appendChild(document.createTextNode("Total Price is: " + getTotalPrice(chosenProducts)+" $"));
+
+	openInfo(event, "Cart");
+
+}
+
+function priceFilter(a,b){
+	return a.price > b.price ? 1:-1;
+}
+
+function renderProductList(optionArray){
+	var s2 = document.getElementById('displayProduct');
+	const sortedOptionArray = optionArray.sort(priceFilter);
+	for (i = 0; i < sortedOptionArray.length; i++) {
+
+		var label = document.createElement('label');
+		label.className = "option_item";
+
+	  var productName = sortedOptionArray[i].name;
+	  var productPrice = sortedOptionArray[i].price;
+		var productPicture = sortedOptionArray[i].picture;
+		var productType = sortedOptionArray[i].type;
+
+		var type = document.createElement('div');
+		type.style.display = "none";
+		type.title = "type";
+		if (productType == "Fruit") {
+			type.className = "Fruits";
+		}
+		if (productType == "Vegetable") {
+			type.className = "Vegetables";
+		}
+		if (productType == "Fat") {
+			type.className = "Fats";
+		}
+		if (productType == "Protein") {
+			type.className = "Proteins";
+		}
+		if (productType == "Carbs") {
+			type.className = "Carbs";
+		}
+		if (productType == "Dairy") {
+			type.className = "Dairy";
+		}
+
+	  var checkbox = document.createElement("input");
+	  checkbox.type = "checkbox";
+		checkbox.className = "checkbox";
+	  checkbox.name = "product";
+	  checkbox.value = productName;
+
+		label.appendChild(checkbox);
+		formattedPrice = (Math.round(productPrice * 100) / 100).toFixed(2);
+
+		var optioninner = document.createElement("div");
+		optioninner.className = "option_inner nutfree";
+		var tick = document.createElement("div");
+		tick.className = "tickmark";
+		var icon = document.createElement("div");
+		icon.className = "icon";
+		var productImage = document.createElement("img");
+		productImage.src = productPicture;
+		productImage.alt = "";
+		icon.appendChild(productImage);
+		var prodtext = document.createElement("div");
+		prodtext.className = "boxtext";
+		var h2 = document.createElement("h2");
+		h2.appendChild(document.createTextNode(`${productName} - $${formattedPrice}`));
+		prodtext.appendChild(h2);
+		optioninner.appendChild(tick);
+		optioninner.appendChild(icon);
+		optioninner.appendChild(prodtext);
+
+		label.appendChild(optioninner);
+		type.appendChild(label);
+	  s2.appendChild(type);
+
+	  s2.appendChild(document.createElement("br"));
+	}
+  }
